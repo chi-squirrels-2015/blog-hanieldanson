f97016734110b4e0c99eed412e6649f6d6a23433
hanieldanson <hanson.e.daniel@gmail.com>
2015-02-22 23:18:47 -0600

    fix config

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     resources :comments
   end
 
-  root 'application#hello'
+  root 'articles#index'
 end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
